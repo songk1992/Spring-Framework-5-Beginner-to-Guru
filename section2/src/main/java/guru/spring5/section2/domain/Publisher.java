d2bd50daae6ec08f743c22dff471cc6a7b9551a0
songk1992-home <zuppiy@naver.com>
2021-09-25 00:34:32 +0900

    Thymeleaf Templates

--- section2/src/main/java/guru/spring5/section2/domain/Publisher.java
+++ section2/src/main/java/guru/spring5/section2/domain/Publisher.java
@@ -43,7 +43,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-                ", books=" + books +
                 '}';
     }
 
