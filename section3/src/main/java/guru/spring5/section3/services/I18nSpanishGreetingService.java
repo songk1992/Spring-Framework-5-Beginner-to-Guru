d37978caef5a9f818e02ebeabb56ffeb87c8eca2
songk1992-home <zuppiy@naver.com>
2021-09-26 12:16:27 +0900

    Default Profile

--- section3/src/main/java/guru/spring5/section3/services/I18nSpanishGreetingService.java
+++ section3/src/main/java/guru/spring5/section3/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package guru.spring5.section3.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ESP")
+@Profile({"ESP", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
