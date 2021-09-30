b0eeed600e480a419fdb6ee9088efe680973561f
songk1992-home <zuppiy@naver.com>
2021-09-30 22:48:49 +0900

    Java Configuration Example

---
+++ section5/src/main/java/org/springframework/boot/section5/config/GreetingServiceConfig.java
@@ -0,0 +1,26 @@
+package org.springframework.boot.section5.config;
+
+import org.springframework.boot.section5.services.ConstructorGreetingService;
+import org.springframework.boot.section5.services.PropertyInjectedGreetingService;
+import org.springframework.boot.section5.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
--- section5/src/main/java/org/springframework/boot/section5/services/SetterInjectedGreetingService.java
+++ section5/src/main/java/org/springframework/boot/section5/services/SetterInjectedGreetingService.java
@@ -2,7 +2,6 @@ package org.springframework.boot.section5.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public class SetterInjectedGreetingService implements GreetingService{
     @Override
     public String sayGreeting(){
