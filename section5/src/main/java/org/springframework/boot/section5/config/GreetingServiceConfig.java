369402e0094d365f777fb3497af69448cce996f3
songk1992-home <zuppiy@naver.com>
2021-10-01 21:38:17 +0900

    Spring XML Configuration Example

--- section5/src/main/java/org/springframework/boot/section5/config/GreetingServiceConfig.java
+++ section5/src/main/java/org/springframework/boot/section5/config/GreetingServiceConfig.java
@@ -5,11 +5,9 @@ import com.springframework.pets.PetServiceFactory;
 import org.springframework.boot.section5.repositories.EnglishGreetingRepository;
 import org.springframework.boot.section5.repositories.EnglishGreetingRepositoryImpl;
 import org.springframework.boot.section5.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
@@ -58,10 +56,10 @@ public class GreetingServiceConfig {
     }
 
 
-    @Bean
-    ConstructorGreetingService constructorGreetingService(){
-        return new ConstructorGreetingService();
-    }
+    //@Bean
+    //ConstructorGreetingService constructorGreetingService(){
+    //    return new ConstructorGreetingService();
+    //    }
 
     @Bean
     PropertyInjectedGreetingService propertyInjectedGreetingService(){
