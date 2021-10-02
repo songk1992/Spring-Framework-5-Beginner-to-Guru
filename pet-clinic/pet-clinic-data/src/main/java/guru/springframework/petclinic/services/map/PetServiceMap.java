7e6896508de4da3972056a6a962f19c57a2dffa1
songk1992-home <zuppiy@naver.com>
2021-10-02 10:17:50 +0900

    Implement Spring Configuration

--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetServiceMap.java
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package guru.springframework.petclinic.services.map;
 
 import guru.springframework.petclinic.model.Pet;
 import guru.springframework.petclinic.services.CrudService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
     @Override
     public Set<Pet> findAll() {
