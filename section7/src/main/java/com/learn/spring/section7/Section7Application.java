849df192c7edf583a045a1bf36141e3270f31c92
songk1992-home <zuppiy@naver.com>
2021-10-04 11:33:37 +0900

    init section7

---
+++ section7/src/main/java/com/learn/spring/section7/Section7Application.java
@@ -0,0 +1,13 @@
+package com.learn.spring.section7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section7Application.class, args);
+    }
+
+}
---
+++ section7/src/test/java/com/learn/spring/section7/Section7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring.section7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
