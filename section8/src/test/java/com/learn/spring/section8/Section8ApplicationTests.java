57537a2c76987ac02adb045627b484c731fd2141
songk1992-home <zuppiy@naver.com>
2021-10-06 23:19:24 +0900

    section 8 init

---
+++ section8/src/test/java/com/learn/spring/section8/Section8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring.section8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
