7142155d7feb958de70ec10ce193968d3bb8e386
songk1992-home <zuppiy@naver.com>
2021-10-07 23:20:26 +0900

    Database Initialization with Spring

--- section8/src/main/java/com/learn/spring/section8/domain/Category.java
+++ section8/src/main/java/com/learn/spring/section8/domain/Category.java
@@ -9,8 +9,32 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String categoryName;
+    private String description;
 
     @ManyToMany(mappedBy = "categories")
-    private Set<Recipe> recipe;
+    private Set<Recipe> recipes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
