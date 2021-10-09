721bd1f510b20dc72602eba54416fe5f54590c3b
songk1992-home <zuppiy@naver.com>
2021-10-09 13:40:28 +0900

    Create Base Entity

--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -1,9 +1,25 @@
 package guru.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+/*
+추가 참조
+MappedSuperclass
+https://www.baeldung.com/hibernate-inheritance
+
+ */
+
+
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
