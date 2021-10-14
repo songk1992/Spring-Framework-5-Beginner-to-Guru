166b22a9c48f39be9883813b110b2bf94ed0a1a5
songk1992-home <zuppiy@naver.com>
2021-10-14 21:42:52 +0900

    Spring Integration Test

---
+++ section10/src/test/java/com/learn/spring/section10/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,44 @@
+package com.learn.spring.section10.repositories;
+
+import com.learn.spring.section10.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.text.html.Option;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/* Integration Test */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("teaspoon uom test")
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    @DisplayName("cup uom test")
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
