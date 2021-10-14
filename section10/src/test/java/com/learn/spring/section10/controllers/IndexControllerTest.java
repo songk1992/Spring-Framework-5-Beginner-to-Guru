c464032cb875e601a924b55a20a7f8d02ab1422b
songk1992-home <zuppiy@naver.com>
2021-10-14 21:03:48 +0900

    Mockito Argument Capture

--- section10/src/test/java/com/learn/spring/section10/controllers/IndexControllerTest.java
+++ section10/src/test/java/com/learn/spring/section10/controllers/IndexControllerTest.java
@@ -1,12 +1,17 @@
 package com.learn.spring.section10.controllers;
 
+import com.learn.spring.section10.domain.Recipe;
 import com.learn.spring.section10.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -22,17 +27,36 @@ class IndexControllerTest {
     RecipeService recipeService;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         controller = new IndexController(recipeService);
     }
 
     @Test
     @DisplayName("콘트롤러가 뷰를 잘 리턴하는지 테스트")
-    public void getIndexPageTest(){
+    public void getIndexPageTest() {
+
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
         String viewName = controller.getIndexPage(model);
+
+        // then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
