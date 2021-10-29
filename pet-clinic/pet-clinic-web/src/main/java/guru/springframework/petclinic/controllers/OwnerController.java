d532dcec980e1d0d44524f67da53433f4f47a150
songk1992-home <zuppiy@naver.com>
2021-10-29 21:34:45 +0900

    Testing Owner Controller with MockMVC closes #8

--- pet-clinic/pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic/pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package guru.springframework.petclinic.controllers;
 import guru.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -11,20 +14,28 @@ public class OwnerController {
 
     private final OwnerService ownerService;
 
-    public OwnerController(OwnerService ownerService){
+    public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index","/index.html"})
-    public String listOwners(Model model){
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
 
-    @RequestMapping("/find" )
-    public String findOwners(){
+    @RequestMapping("/find")
+    public String findOwners() {
         return "notimplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 }
