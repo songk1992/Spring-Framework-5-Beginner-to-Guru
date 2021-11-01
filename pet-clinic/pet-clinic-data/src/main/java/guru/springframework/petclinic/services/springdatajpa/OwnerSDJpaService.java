5e2bafbf6a17cc556213df91e2197d2853551255
songk1992-home <zuppiy@naver.com>
2021-11-01 21:39:53 +0900

    Implement Find Owner Closes #10

--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -24,6 +25,15 @@ public class OwnerSDJpaService implements OwnerService {
         this.ownerRepository = ownerRepository;
         this.petRepository = petRepository;
         this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName){
+        return ownerRepository.findAllByLastNameLike(lastName);
     }
 
     @Override
@@ -53,8 +63,5 @@ public class OwnerSDJpaService implements OwnerService {
         ownerRepository.deleteById(aLong);
     }
 
-    @Override
-    public Owner findByLastName(String lastName) {
-        return ownerRepository.findByLastName(lastName);
-    }
+
 }
--- pet-clinic/pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic/pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
@@ -1,14 +1,19 @@
 package guru.springframework.petclinic.controllers;
 
+import guru.springframework.petclinic.model.Owner;
 import guru.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/owners")
 @Controller
@@ -21,21 +26,46 @@ public class OwnerController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder){
+    public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(Model model) {
-
-        model.addAttribute("owners", ownerService.findAll());
-
-        return "owners/index";
-    }
+//    @RequestMapping({"", "/", "/index", "/index.html"})
+//    public String listOwners(Model model) {
+//
+//        model.addAttribute("owners", ownerService.findAll());
+//
+//        return "owners/index";
+//    }
 
     @RequestMapping("/find")
-    public String findOwners() {
-        return "notimplemented";
+    public String findOwners(Model model) {
+        model.addAttribute("owner", Owner.builder().build());
+        return "owners/findOwners";
+    }
+
+    @RequestMapping
+    public String processFindForm(Owner owner, BindingResult result, Map<String, Object> model) {
+        // allow parameterless GET request for /owners to return all records
+        if (owner.getLastName() == null) {
+            owner.setLastName(""); // empty string signifies broadest possible reach
+        }
+
+        // find owners bt last name
+        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        if (results.isEmpty()) {
+            // no owners found
+            result.rejectValue("lastName", "notFound", "not found");
+            return "owners/findOwners";
+        } else if (results.size() == 1) {
+            // 1 owner found
+            owner = results.get(0);
+            return "redirect:/owners/" + owner.getId();
+        } else {
+            // multiple owners found
+            model.put("selections", results);
+            return "owners/ownersList";
+        }
     }
 
     @GetMapping("/{ownerId}")
